b588d887aba08c130ffdc3b28d81159dfe3070fa
Francky <gwadafrank@gmail.com>
2020-06-12 13:16:10 +0300

    (faeat)-add goals, list  and display descriptions

---
+++ src/app/components/goal.ts
@@ -0,0 +1,12 @@
+export class Goal {
+
+    constructor(
+        public id:number,
+        public name: string,
+        public description: string,
+        public completeDate: Date
+    ){
+
+    }
+
+}
