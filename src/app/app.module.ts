cf88e878fb995ddf30b9cd1c8032d68cc6a20095
Francky <gwadafrank@gmail.com>
2020-06-13 14:36:06 +0300

    (feat):  display details in seperate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalListComponent } from './components/goal-list/goal-list.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { GoalDetailsComponent } from './components/goal-details/goal-details.component'; // <-- NgModel lives here
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalListComponent
+    GoalListComponent,
+    GoalDetailsComponent
   ],
   imports: [
     BrowserModule,
